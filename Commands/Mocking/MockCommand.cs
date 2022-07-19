e8d9e1744967b962d64500e51a8e2f2a59a07517
Alex.Baker@coxautoinc.com <Alex.Baker@coxautoinc.com>
2022-07-19 17:49:34 -0500

    sacrastifying text;updated gitignore

--- Commands/Mocking/MockCommand.cs
+++ Commands/Mocking/MockCommand.cs
@@ -11,6 +11,13 @@ namespace Dolores.Commands.Mocking
 {
     public class MockCommand : BaseCommandModule
     {
+        private readonly Utility _utility;
+
+        public MockCommand()
+        {
+            _utility = new Utility();
+        }
+
         [Command("ping")] // let's define this method as a command
         [Description("Example ping command")] // this will be displayed to tell users what this command does when they invoke help
         [Aliases("pong")] // alternative names for the command
@@ -27,5 +34,24 @@ namespace Dolores.Commands.Mocking
             await ctx.RespondAsync($"{emoji} Pong! Ping: {ctx.Client.Ping}ms");
         }
 
+        [Command("mock")] // let's define this method as a command
+        [Description("Mocks the tagged person")] // this will be displayed to tell users what this command does when they invoke help
+        [Aliases("youSuck")] // alternative names for the command
+        public async Task Mock(CommandContext ctx, DiscordMember member) // this command takes no arguments
+        {
+            // let's trigger a typing indicator to let
+            // users know we're working
+            await ctx.TriggerTypingAsync();
+            var message = await _utility.GetLastMessageAsync(ctx, member);
+
+            if(message == null)
+            {
+                await ctx.RespondAsync($"Couldn't find a message for {member.DisplayName}");
+            }
+            var sarcasticMessage = $"{_utility.Sarcastify(message)} {member.Mention}";
+
+            // respond with ping
+            await ctx.RespondAsync(sarcasticMessage);
+        }
     }
 }
---
+++ Utility.cs
@@ -0,0 +1,41 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolores
+{
+    public class Utility
+    {
+        public async Task<string> GetLastMessageAsync(CommandContext ctx, DiscordMember member)
+        {
+            var messages = await ctx.Channel.GetMessagesAsync(100);
+            messages = messages.ToList();
+            return messages.Where(message => message.Author.Id.Equals(member.Id))?.FirstOrDefault(message => message.Content != ".")?.Content;
+        }
+
+        public string Sarcastify(string word)
+        {
+            word = word.ToLower();
+            var newWord = "";
+            for (var i = 0; i < word.Length; i++)
+            {
+                var letter = word[i];
+                if (i % 2 == 0)
+                {
+                    letter = Char.ToUpper(letter);
+                }
+
+                newWord += letter;
+            }
+
+            newWord += " ";
+
+            return newWord;
+        }
+
+    }
+}
