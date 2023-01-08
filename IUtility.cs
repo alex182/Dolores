d668f24474ad6ca24f5e9b78d653bf6c3373e554
alex182 <alexbaker182@gmail.com>
2023-01-08 15:27:02 -0600

    removed speaker vote

--- IUtility.cs
+++ IUtility.cs
@@ -1,6 +1,5 @@
 ﻿using Dolores.Clients.Models;
 using Dolores.Clients.RocketLaunch.Models.RocketLaunchLive.Response;
-using Dolores.Commands.NytSpeaker.Model;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.Entities;
 
@@ -12,7 +11,6 @@ namespace Dolores
         Task<string> RandomInsult(string name);
         string Sarcastify(string word);
         Task<string> GetSlogan(string sloganWord);
-        Task<string>GetVote();
         Task<APIResultsWrapper<ResponseBody>> GetLaunches(DateTime? startDate, DateTime? endDate);
         Task SendLaunchNotification(ResponseBody launchInfo);
     }
